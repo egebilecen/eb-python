a7b756e0e0e09d804e7231798ebb66c2a9fe738d
Ege Bilecen <egebilecen@icloud.com>
2021-08-14 13:03:26 +0300

    fixed variable names

--- eb/sensor/mpu.py
+++ eb/sensor/mpu.py
@@ -37,45 +37,45 @@ class MPU:
         while cls._status:
             try:
                 ### Accelometer / Gyroscope
-                block_data = cls._gyro_acc.read_block_data(HW290.REGISTER.STATUS, 1)
+                block_data = cls._gyro_acc.read_block_data(MPU.REGISTER.STATUS, 1)
 
                 if not block_data[0] & 0x01:
                     continue
 
                 # Accel data
-                block_data = cls._gyro_acc.read_block_data(HW290.REGISTER.ACCEL_DATA, 6)
+                block_data = cls._gyro_acc.read_block_data(MPU.REGISTER.ACCEL_DATA, 6)
                 
                 accel_data = {
                     "raw": {
-                        "x" : HW290._parse_gyro_acc_val((block_data[0] << 8) | block_data[1]),
-                        "y" : HW290._parse_gyro_acc_val((block_data[2] << 8) | block_data[3]),
-                        "z" : HW290._parse_gyro_acc_val((block_data[4] << 8) | block_data[5])
+                        "x" : MPU._parse_gyro_acc_val((block_data[0] << 8) | block_data[1]),
+                        "y" : MPU._parse_gyro_acc_val((block_data[2] << 8) | block_data[3]),
+                        "z" : MPU._parse_gyro_acc_val((block_data[4] << 8) | block_data[5])
                     },
                     "scaled": {}
                 }
 
                 accel_data["scaled"] = {
-                    "x" : HW290._parse_gyro_acc_val(accel_data["raw"]["x"]) / 2048.,
-                    "y" : HW290._parse_gyro_acc_val(accel_data["raw"]["y"]) / 2048.,
-                    "z" : HW290._parse_gyro_acc_val(accel_data["raw"]["z"]) / 2048.
+                    "x" : MPU._parse_gyro_acc_val(accel_data["raw"]["x"]) / 2048.,
+                    "y" : MPU._parse_gyro_acc_val(accel_data["raw"]["y"]) / 2048.,
+                    "z" : MPU._parse_gyro_acc_val(accel_data["raw"]["z"]) / 2048.
                 }
 
                 # Gyro data
-                block_data = cls._gyro_acc.read_block_data(HW290.REGISTER.GYRO_DATA, 6)
+                block_data = cls._gyro_acc.read_block_data(MPU.REGISTER.GYRO_DATA, 6)
                 
                 gyro_data = {
                     "raw": {
-                        "x" : HW290._parse_gyro_acc_val((block_data[0] << 8) | block_data[1]),
-                        "y" : HW290._parse_gyro_acc_val((block_data[2] << 8) | block_data[3]),
-                        "z" : HW290._parse_gyro_acc_val((block_data[4] << 8) | block_data[5]),
+                        "x" : MPU._parse_gyro_acc_val((block_data[0] << 8) | block_data[1]),
+                        "y" : MPU._parse_gyro_acc_val((block_data[2] << 8) | block_data[3]),
+                        "z" : MPU._parse_gyro_acc_val((block_data[4] << 8) | block_data[5]),
                     },
                     "scaled": {}
                 }
 
                 gyro_data["scaled"] = {
-                    "x" : HW290._parse_gyro_acc_val(gyro_data["raw"]["x"]) / 16.4,
-                    "y" : HW290._parse_gyro_acc_val(gyro_data["raw"]["y"]) / 16.4,
-                    "z" : HW290._parse_gyro_acc_val(gyro_data["raw"]["z"]) / 16.4
+                    "x" : MPU._parse_gyro_acc_val(gyro_data["raw"]["x"]) / 16.4,
+                    "y" : MPU._parse_gyro_acc_val(gyro_data["raw"]["y"]) / 16.4,
+                    "z" : MPU._parse_gyro_acc_val(gyro_data["raw"]["z"]) / 16.4
                 }
 
                 cls._last_data = {
@@ -89,15 +89,15 @@ class MPU:
 
     def start(self) -> None:
         ### Accelometer / Gyroscope settings
-        self._gyro_acc.write_byte(HW290.REGISTER.PWR_MGMT_1, 0x00)
+        self._gyro_acc.write_byte(MPU.REGISTER.PWR_MGMT_1, 0x00)
 
         # Accel Sensitivity
         # 0, 8, 16 and 24 (int) for 16384, 8192, 4096 and 2048 sensitivity respectively
-        self._gyro_acc.write_byte(HW290.REGISTER.ACCEL_SENSITIVITY, 0x18)
+        self._gyro_acc.write_byte(MPU.REGISTER.ACCEL_SENSITIVITY, 0x18)
 
         # Gyro Sensitivity
         # 0, 8, 16 and 24 (int) for 131, 65.5, 32.8 and 16.4 sensitivity respectively
-        self._gyro_acc.write_byte(HW290.REGISTER.GYRO_SENSITIVITY, 0x18)
+        self._gyro_acc.write_byte(MPU.REGISTER.GYRO_SENSITIVITY, 0x18)
 
         # Magnetometer
         # Enable
