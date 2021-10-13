097c5b81495e86558b35a07d3f036de3148483a0
Ege Bilecen <egebilecen@icloud.com>
2021-10-13 17:35:25 +0300

    added method to decode encoded image

--- eb/image_processing/image.py
+++ eb/image_processing/image.py
@@ -3,6 +3,7 @@
     Date  : 04.08.2020
 """
 import cv2
+import numpy as np
 
 class Image:
     class Encode:
@@ -11,3 +12,8 @@ class Image:
                          extension : str = ".jpg") -> bytes:
             _, encoded_frame = cv2.imencode(extension, frame)
             return encoded_frame.tobytes()
+
+    class Decode:
+        @staticmethod
+        def FromEncodedImage(frame : bytes):
+            return cv2.imdecode(np.frombuffer(frame, np.uint8), -1)
