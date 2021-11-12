99ced8a0332a4ab06883847f6ed7c42211037567
Ege Bilecen <egebilecen@icloud.com>
2021-11-12 11:06:01 +0300

    added channel constants to helper

--- eb/mavlink/helper.py
+++ eb/mavlink/helper.py
@@ -4,8 +4,22 @@
 """
 from pymavlink import mavutil
 
-def get_flight_modes(mavlink):
-    return mavlink.mode_mapping()
+class Copter:
+    class Channel:
+        ROLL        = 1
+        PITCH       = 2
+        THROTTLE    = 3
+        YAW         = 4
+        FLIGHT_MODE = 5
+        OPTIONAL_1  = 6
+        AUX_1       = 7
+        AUX_2       = 8
+        AUX_3       = 9
+        AUX_4       = 10
+        AUX_5       = 11
+        AUX_6       = 12
+        AUX_7       = 13
+        AUX_8       = 14
 
 class Enum:
     @staticmethod
@@ -15,3 +29,6 @@ class Enum:
     @staticmethod
     def get_method_reference(mav, msg_str):
         return getattr(mav, msg_str.lower()+"_send")
+
+def get_flight_modes(mavlink):
+    return mavlink.mode_mapping()
